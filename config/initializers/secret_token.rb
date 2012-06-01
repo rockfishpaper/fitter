f1d6f35b2c9b3863fb7fdd1f24cfc4106ccec5aa
Patrick Yan <dev@yansolutions.com>
2012-06-01 12:11:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitterApp::Application.config.secret_token = 'f12e11ac911020d5dc0e5a6e1aaf6a084ddeade364b01e0df66b02819786d45d5561538907eec76fd4524bb766f72f80330a205e3c1b99899348e4673bb46fb9'
