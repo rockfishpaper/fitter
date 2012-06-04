e1e8ec97f95138d4f4ef7be2520668fb41016557
Patrick Yan <dev@yansolutions.com>
2012-06-04 00:25:31 -0700

    Fixed public files override

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 FitterApp::Application.routes.draw do
   root to: 'static_pages#home'
-  
-  match '/help',    to: "static_pages#help"
 
-  match '/about',   to: "static_pages#about"
 
-  match '/contact', to: "static_pages#contact"
+  match '/about',   to: 'static_pages#about' 
+  match '/help',    to: 'static_pages#help'
+  match '/contact', to: 'static_pages#contact'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
