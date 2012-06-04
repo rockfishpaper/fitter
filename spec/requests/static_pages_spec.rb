f6c3994f3f46ddf227ffe672a8febf616dd4fa0f
Patrick Yan <dev@yansolutions.com>
2012-06-04 00:11:33 -0700

    Templates and Styles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,18 +5,18 @@ describe "Static pages" do
   describe "Home page" do
   	
     it "should have the content 'Fitter App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Fitter App')
+      visit root_path
+      page.should have_selector('h1', text: 'Fitter App')
     end
 
     it "should have the base title 'Fitter App'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Fitter App")
+      visit root_path
+      page.should have_selector('title', text: "Fitter App")
     end
     
     it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
+      visit root_path
+      page.should_not have_selector('title', text: '| Home')
     end
 
   end
@@ -24,13 +24,13 @@ describe "Static pages" do
   describe "Help page" do
   	
     it "should have the content 'Help'" do
-  	  visit '/static_pages/help'
-  	  page.should have_selector('h1', :text => 'Help')
+  	  visit help_path
+  	  page.should have_selector('h1', text: 'Help')
   	end
     
     it "should have the right title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Fitter App | Help")
+      visit help_path
+      page.should have_selector('title', text: "Fitter App | Help")
     end
     
   end
@@ -38,13 +38,13 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
 
-    it "should have the right title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Fitter App | About")
+    it "should have the right title 'About Us'" do
+      visit about_path
+      page.should have_selector('title', text: "Fitter App | About Us")
     end
 
   end
@@ -52,13 +52,13 @@ describe "Static pages" do
   describe "Contact page" do
   
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
     end
     
     it "should have the right title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Fitter App | Contact")
+      visit contact_path
+      page.should have_selector('title', text: "Fitter App | Contact")
     end
   end
 
