c53349abac09008dc75731db7aa932e006fcadce
Patrick Yan <dev@yansolutions.com>
2012-06-04 11:10:28 -0700

    Finish User Signup Step 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 FitterApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
 
+  match '/signup',  to: 'users#new'
 
   match '/about',   to: 'static_pages#about' 
   match '/help',    to: 'static_pages#help'
